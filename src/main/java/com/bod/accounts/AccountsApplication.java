bc7ede30d10781f0374f8a93a08dc0f433dd787e
Hemant-SE <hemant.raghav@capgemini.com>
2025-01-17 14:31:25 +0530

    Created entities, repositories,services and conroller classes

--- src/main/java/com/bod/accounts/AccountsApplication.java
+++ src/main/java/com/bod/accounts/AccountsApplication.java
@@ -1,12 +1,31 @@
 package com.bod.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Bank of Delhi Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hemant raghav",
+						email = "hemant@gmail.com"
+				)
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AccountsApplication.class, args);
 	}
 
